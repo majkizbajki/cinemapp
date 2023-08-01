2928b293356f2f3bbce6f1bfb130acc2731fbe2b
Michał <79280309+majkizbajki@users.noreply.github.com>
2023-08-01 18:39:04 +0200

    feat/Add i18n (#3)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigation } from './src/navigation';
 import { ThemeProvider } from './src/context/ThemeContext';
+import './src/i18n/i18n';
 
 function App(): JSX.Element {
     return (
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,30 @@
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import en from './locales/en.json';
+import pl from './locales/pl.json';
+
+export const resources = {
+    en: {
+        translations: en
+    },
+    pl: {
+        translations: pl
+    }
+};
+
+export const defaultNS = 'translations';
+
+i18n.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: [defaultNS],
+    resources,
+    defaultNS,
+
+    interpolation: {
+        escapeValue: false
+    }
+});
+
+export default i18n;
